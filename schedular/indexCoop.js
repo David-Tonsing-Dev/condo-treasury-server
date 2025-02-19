f7b8d3e7ec52e0e767a4537d0f975e6de3fc5143
unknown <ktaher642@gmail.com>
2025-02-19 16:39:51 +0530

    minor bug

--- schedular/indexCoop.js
+++ schedular/indexCoop.js
@@ -3,10 +3,14 @@ const axios = require("axios");
 
 let cronRun = 0;
 
-cron.schedule("*/1 * * * *", async () => {
-  cronRun++;
-  const indexCoopDetail = await axios.get(
-    "https://api.indexcoop.com/data/tokens/0xc884646e6c88d9b172a23051b38b0732cc3e35a6?metrics=supply&metrics=nav"
-  );
-  console.log("indexCoopDetail", indexCoopDetail.data, "cronRun", cronRun);
-});
+const runCronJobs = () => {
+  cron.schedule("*/1 * * * *", async () => {
+    cronRun++;
+    const indexCoopDetail = await axios.get(
+      "https://api.indexcoop.com/data/tokens/0xc884646e6c88d9b172a23051b38b0732cc3e35a6?metrics=supply&metrics=nav"
+    );
+    console.log("indexCoopDetail", indexCoopDetail.data, "cronRun", cronRun);
+  });
+};
+
+module.exports = runCronJobs;
