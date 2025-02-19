0d06ec83ca73817dfed72025264255599a5b85d8
unknown <ktaher642@gmail.com>
2025-02-19 15:34:27 +0530

    checking cron running or not in heroku

---
+++ schedular/indexCoop.js
@@ -0,0 +1,12 @@
+const cron = require("node-cron");
+const axios = require("axios");
+
+let cronRun = 0;
+
+cron.schedule("*/1 * * * *", async () => {
+  cronRun++;
+  const indexCoopDetail = await axios.get(
+    "https://api.indexcoop.com/data/tokens/0xc884646e6c88d9b172a23051b38b0732cc3e35a6?metrics=supply&metrics=nav"
+  );
+  console.log("indexCoopDetail", indexCoopDetail.data, "cronRun", cronRun);
+});
